6be62235d9e899a6f998c4effc3d7fab5919a7b3
Kevin Yunas <kevinyunas@icloud.com>
2022-01-20 13:14:55 -0800

    Created main finnhub api client

--- frontend/src/API/finnhub_api.js
+++ frontend/src/API/finnhub_api.js
@@ -1,3 +1,8 @@
 import keys from '../keys.js'
 const finnhub = require("finnhub");
 
+const api_key = finnhub.ApiClient.instance.authentications['api_key'];
+api_key.api_key = keys.finnhub_key;
+const finnhub_client = new finnhub.DefaultApi();
+
+export default finnhub_client;
